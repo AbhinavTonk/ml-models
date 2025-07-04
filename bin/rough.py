648a2d4a9fa060eb647f94ca64b3c9ad208237a2
unknown <abhinavtonk@gmail.com>
2025-07-05 00:00:13 +0530

    Renaming the script name

---
+++ bin/rough.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4]
+y=[10,20,30,40]
+# plt.plot(x,y,label='Line 1')
+# plt.plot(y,x,label='Line 2')
+# plt.title("my plot")
+# plt.xlabel("X Axis")
+# plt.ylabel("Y Axis")
+# plt.legend()
+# plt.show()
+
+plt.pie(x)
+plt.show()
